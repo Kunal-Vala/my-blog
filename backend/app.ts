baf6d71fce405a495d3995565fc7cf564f606756
Kunal-Vala <kunalvala2005@gmail.com>
2025-12-13 22:25:48 +0530

    use : post router

--- backend/app.ts
+++ backend/app.ts
@@ -1,8 +1,10 @@
 import express from "express"
-
+import {router as postRouter} from './routes/posts'
 const app = express()
 
 app.use(express.json())
+
+app.use('/post',postRouter)
 
 app.get('/',(req,res)=>{
     res.send('SERVER RUNNING')
