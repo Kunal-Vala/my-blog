8a1ef48227240c8fa83d21de6edb325c91104e40
Kunal-Vala <kunalvala2005@gmail.com>
2025-12-13 22:52:55 +0530

    feat : createPost

--- backend/controllers/postController.ts
+++ backend/controllers/postController.ts
@@ -29,3 +29,36 @@ export const getPostById = async (req: Request, res: Response) => {
         res.status(500).json({ message: "Failed to fetch posts" })
     }
 }
+
+export const createPost = async (req: Request, res: Response) => {
+    try {
+        const { title, content, published } = req.body
+        if (!title || !content) {
+            return res.status(400).json({
+                message: "Title and content are required",
+            })
+        }
+
+        const slug = title
+            .toLowerCase()
+            .trim()
+            .replace(/[^a-z0-9]+/g, "-")
+            .replace(/(^-|-$)/g, "")
+
+
+        const post = await prisma.post.create({
+            data: {
+                title,
+                slug,
+                content,
+                published: Boolean(published),
+                publishedAt: published ? new Date() : null,
+            },
+        })
+
+        res.status(201).json(post)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: "Failed to create post" })
+    }
+}
--- backend/routes/posts.ts
+++ backend/routes/posts.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 const router = Router()
-import { getPosts,getPostById } from '../controllers/postController'
+import { getPosts,getPostById,createPost } from '../controllers/postController'
 
 router.get('/', getPosts)
+router.post('/', createPost)
 router.get('/:id', getPostById)
 
 export { router }
