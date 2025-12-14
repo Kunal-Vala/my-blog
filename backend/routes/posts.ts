2dcb21d16426d103dfcb98e6447522e0e84ba871
Kunal-Vala <kunalvala2005@gmail.com>
2025-12-14 22:03:50 +0530

    feat : authenticate middleware used

--- backend/routes/posts.ts
+++ backend/routes/posts.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 const router = Router()
 import { getPosts,getPostById,createPost } from '../controllers/postController'
+import { authenticate } from "../middlewares/auth.middleware";
 
-router.get('/', getPosts)
-router.post('/', createPost)
+router.get('/',authenticate, getPosts)
+router.post('/', authenticate, createPost)
 router.get('/:id', getPostById)
 
 export { router }
