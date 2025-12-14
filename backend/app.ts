a3d1fc2fcbbdfde87f3cbdd4d2a21e27c8f3cbbc
Kunal-Vala <kunalvala2005@gmail.com>
2025-12-14 15:03:11 +0530

    add : auth route

--- backend/app.ts
+++ backend/app.ts
@@ -1,13 +1,15 @@
 import express from "express"
-import {router as postRouter} from './routes/posts'
+import { router as postRouter } from './routes/posts'
+import { router as authRouter } from './routes/auth'
 const app = express()
 
 app.use(express.json())
 
-app.use('/post',postRouter)
+app.use('/post', postRouter)
+app.use('/auth', authRouter)
 
-app.get('/',(req,res)=>{
-    res.send('SERVER RUNNING')
+app.get('/', (req, res) => {
+  res.send('SERVER RUNNING')
 
 })
 
