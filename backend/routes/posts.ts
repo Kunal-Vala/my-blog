4ad2ad0e12bec1230741afb237e41928e3ffcc72
Kunal-Vala <kunalvala2005@gmail.com>
2025-12-15 15:58:27 +0530

    feat : validate body for createPost

--- backend/routes/posts.ts
+++ backend/routes/posts.ts
@@ -2,10 +2,35 @@ import { Router } from "express";
 const router = Router()
 import { getPosts, getPostById, createPost, updatePost } from '../controllers/postController'
 import { authenticate, authorizeRoles, checkPostOwnership } from "../middlewares/auth.middleware";
+import { validate } from "../middlewares/validate";
+import { CreatePostSchema } from "../schemas/post.schema";
 
-router.get('/', authenticate, authorizeRoles(["USER", "AUTHOR", "ADMIN" ]), getPosts)
-router.post('/', authenticate, authorizeRoles(["AUTHOR", "ADMIN"]), createPost)
-router.put('/:id', authenticate, authorizeRoles(["AUTHOR", "ADMIN"]), checkPostOwnership, updatePost)
-router.get('/:id', getPostById)
+router.get(
+    '/',
+    authenticate,
+    authorizeRoles(["USER", "AUTHOR", "ADMIN"]),
+    getPosts
+)
+
+router.post(
+    '/',
+    authenticate,
+    authorizeRoles(["AUTHOR", "ADMIN"]),
+    validate(CreatePostSchema,'body'),
+    createPost
+)
+
+router.put(
+    '/:id',
+    authenticate,
+    authorizeRoles(["AUTHOR", "ADMIN"]),
+    checkPostOwnership,
+    updatePost
+)
+
+router.get(
+    '/:id',
+    getPostById
+)
 
 export { router }
